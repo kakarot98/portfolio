9236a286c30620ea69f776bc7c961c9fa86c8c28
Ashish's ASUS laptop <hrushikeshmohanty90@gmail.com>
2025-02-01 19:24:57 -0500

    changed styling of links

--- src/components/education/Card.jsx
+++ src/components/education/Card.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+
+const baseCardClass = "bg-white shadow-md rounded-lg p-6 mb-4 transition-transform duration-300";
+const hoverClass = "hover:scale-105 hover:shadow-xl";
+const dimensionClass = "w-72 h-80"; // Customize width and height as needed
+
 function Card({ institution, degree, duration, coursework }) {
     return (
         <div className="bg-white shadow-md rounded-lg p-6 mb-4" style={{ backgroundColor: '#f5f5f5' }}>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 import Education from '../education/Education';
 import Experience from '../experience/Experience';
 import Skills from '../skills/Skills';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Project from '../projects/Project';
 
+const navLinkClass = ({ isActive }) =>
+  isActive
+    ? 'text-blue-500 '
+    : 'hover:text-blue-500 transition-colors duration-200'
+
 const Navbar = () => {
+  
+
   return (
     <div className='flex flex-row flex-wrap justify-between items-center bg-slate-200 shadow-md p-2'>
-      <h1 className='text-2xl ml-20 font-semibold hover:text-blue-500'><Link to={``}>Profile</Link></h1>
+      <h1 className='text-2xl ml-20 font-semibold hover:text-blue-500'><NavLink to={``}>Profile</NavLink></h1>
       <div className='p-4 mr-20 flex flex-row justify-start gap-4 font-medium' >
-        <ul><Link to={'education'} className="hover:text-blue-500">Education</Link></ul>
-        <ul><Link to={'experience'} className="hover:text-blue-500">Experience</Link></ul>
-        <ul><Link to={`project`} className="hover:text-blue-500">Projects</Link></ul>
-        <ul><Link to={'skills'} className="hover:text-blue-500">Skills</Link></ul>
+        <ul><NavLink to={'education'} className={navLinkClass}>Education</NavLink></ul>
+        <ul><NavLink to={'experience'} className={navLinkClass}>Experience</NavLink></ul>
+        <ul><NavLink to={`project`} className={navLinkClass}>Projects</NavLink></ul>
+        <ul><NavLink to={'skills'} className={navLinkClass}>Skills</NavLink></ul>
       </div>
     </div>
   )
